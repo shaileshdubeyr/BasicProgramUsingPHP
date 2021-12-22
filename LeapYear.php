9986e96907e3efe2ede18bb29095b08c6b62666b
SHAILESH <shaileshdubeyr@gmail.com>
2021-12-22 09:30:21 +0530

    Added : code to check leap year

---
+++ LeapYear.php
@@ -0,0 +1,12 @@
+<?php
+$year = readline("Enter the year in 4 digit :- ");
+
+if($year > 999 && $year <= 9999 ){          //condition for 4 digit number
+    if($year % 4 == 0 )                    // checking  leap year or not
+        echo "$year :- Is leap year";
+    else
+        echo "$year :- Is not leap year";
+}
+else
+    echo "Entered wrong input"
+?>
