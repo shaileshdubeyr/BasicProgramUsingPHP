7582ee33fde52df7b89e91897ebbce9a3e76f0e7
SHAILESH <shaileshdubeyr@gmail.com>
2021-12-22 09:58:08 +0530

    Added : code to find quotient and remiander

---
+++ QuotientAndRemainder.php
@@ -0,0 +1,8 @@
+<?php
+    $divident = readline("Enter the divident :- ");
+    $divisor = readline("Enter the number divisor:- ");
+    $quotient = $divident / $divisor;                   //calculating Quotient
+    $remainder = $divident % $divisor;                  //calculating Remainder
+    echo "Input divident is $divident Input divisor is $divisor\n";
+    echo "The quotient is $quotient\nThe remainder is $remainder\n";            
+?>
